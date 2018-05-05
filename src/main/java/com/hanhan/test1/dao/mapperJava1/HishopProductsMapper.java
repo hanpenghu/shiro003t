e399b51446481d225e8898dab493abff564a93ca
hanpenghu <563909408@qq.com>
2018-05-05 18:56:18 +0800

    2018_5_5   weekday(6)   18:55:27

--- src/main/java/com/hanhan/test1/dao/mapperJava1/HishopProductsMapper.java
+++ src/main/java/com/hanhan/test1/dao/mapperJava1/HishopProductsMapper.java
@@ -2,10 +2,15 @@ package com.hanhan.test1.dao.mapperJava1;
 
 import com.hanhan.test1.dto.HishopProducts;
 import com.hanhan.test1.dto.HishopProductsExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface HishopProductsMapper {
+
+    
+
+
     int countByExample(HishopProductsExample example);
 
     int deleteByExample(HishopProductsExample example);
